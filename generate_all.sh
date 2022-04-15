7f88e334f4c4fdf17853b8ff45076645d5f00836
Naegles <na3gles@gmail.com>
2022-04-15 23:37:48 +0200

    Delete generate_all.sh

--- generate_all.sh
+++
@@ -1,5 +0,0 @@
-python3 compile_event.py scripts/event/umi1
-python3 compile_event.py scripts/chara/chara032009
-python3 compile_event.py scripts/chara/chara452001
-python3 compile_event.py scripts/chara/chara462001
-python3 compile_event.py scripts/chara/chara472001
